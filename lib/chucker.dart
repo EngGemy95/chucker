b38f2eae454cf4bead5b4a23752184580b771c21
ArabDT <mgamal@arabdt.com>
2025-07-09 14:23:29 +0300

    Update chucker.dart

--- lib/chucker.dart
+++ lib/chucker.dart
@@ -3,4 +3,4 @@ library chucker;
 export 'core/di.dart' show initChuckerFlutter;
 export 'core/notification_helper.dart' show NotificationHelper;
 export 'presentation/pages/logs_page.dart' show LogsPage;
-export 'core/navigation_service.dart' show navigatorKey;
+export 'core/navigation_service.dart' show NavigationService;
